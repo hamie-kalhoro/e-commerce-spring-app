d4f2fb68d4aa5c5305ec758593dd5f7720fcb8f3
Hamid Ali Kalhoro <hamidKalhoro.in@gmail.com>
2025-06-16 01:21:37 +0500

    complete customer microservice is added with global exception handler

---
+++ services/customer/src/main/java/dev/hamidz/ecommerce/customer/CustomerMapper.java
@@ -0,0 +1,30 @@
+package dev.hamidz.ecommerce.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerMapper {
+
+    public Customer toCustomer(CustomerRequest request) {
+        if (request == null) {
+            return null;
+        }
+        return Customer.builder()
+                .id(request.id())
+                .firstname(request.firstName())
+                .lastname(request.lastName())
+                .email(request.email())
+                .address(request.address())
+                .build();
+    }
+
+    public CustomerResponse fromCustomer(Customer customer) {
+        return new CustomerResponse(
+                customer.getId(),
+                customer.getFirstname(),
+                customer.getLastname(),
+                customer.getEmail(),
+                customer.getAddress()
+        );
+    }
+}
---
+++ services/customer/src/main/java/dev/hamidz/ecommerce/customer/CustomerRequest.java
@@ -0,0 +1,17 @@
+package dev.hamidz.ecommerce.customer;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+public record CustomerRequest(
+        String id,
+        @NotNull(message = "Customer firstname is required")
+        String firstName,
+        @NotNull(message = "Customer secondname is required")
+        String lastName,
+        @NotNull(message = "Customer email is required")
+        @Email(message = "Customer email is not a valid email address")
+        String email,
+        Address address
+) {
+}
